1affd5f3531953b0d16b8de5b5512153c54a58d9
daniellevin76 <Daniel.levin76@outlook.com>
2021-10-22 10:40:59 +0200

    mlkitActivity
    MlKitActivity added

---
+++ app/src/main/java/io/gritacademy/textrecognitionml/MlKitActivity.java
@@ -0,0 +1,14 @@
+package io.gritacademy.textrecognitionml;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MlKitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ml_kit);
+    }
+}
