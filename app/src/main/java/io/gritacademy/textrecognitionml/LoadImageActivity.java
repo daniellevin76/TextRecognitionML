be7675cec31b9da7a22b7394bc83226b9b14e8d4
daniellevin76 <Daniel.levin76@outlook.com>
2021-11-01 17:35:05 +0100

    working
    No cameraX app but everything else works

--- app/src/main/java/io/gritacademy/textrecognitionml/LoadImageActivity.java
+++ app/src/main/java/io/gritacademy/textrecognitionml/LoadImageActivity.java
@@ -1,20 +1,37 @@
 package io.gritacademy.textrecognitionml;
 
+import static com.google.mlkit.vision.common.InputImage.fromBitmap;
+
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 public class LoadImageActivity extends AppCompatActivity {
 
     ImageView imageView;
-    Button loadButton;
+    Button loadButton, getTextButton;
+    InputImage image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +40,28 @@ public class LoadImageActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.loadImageView);
         loadButton = findViewById(R.id.loadImageBtn);
+        getTextButton = findViewById(R.id.getTextBtn);
+
         loadButton.setOnClickListener(v -> {
             mGetContent.launch("image/*");
         });
 
 
+        getTextButton.setOnClickListener(v -> {
+            Log.d("TAG", "oCLICK: ");
+
+            try {
+                image = getImageFromImageView(imageView);
+                recognizeText(image);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        });
+
     }
+
 
     ActivityResultLauncher<String> mGetContent = registerForActivityResult(
             new ActivityResultContracts.GetContent(),
@@ -52,5 +85,66 @@ public class LoadImageActivity extends AppCompatActivity {
             //Log.d("tag", pathStr);
         }
     }
+
+
+    private InputImage getImageFromImageView(ImageView imageView) {
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+
+        return fromBitmap(drawable.getBitmap(), 0);
+    }
+
+    public void switchActivity(String textToSend){
+       // Log.d("TAG", "+-+--+--+-+-+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+     //   Log.d("TAG", "I switchActivity");
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("TEXT_TO_READ", textToSend);
+
+     startActivity(intent);
+    }
+
+
+    private void recognizeText(InputImage image) {
+        Log.d("TAG", "I recognizeText");
+
+        // [START get_detector_default]
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        // [END get_detector_default]
+
+        // [START run_detector]
+        Task<Text> result =
+                recognizer.process(image)
+                        .addOnSuccessListener(visionText -> {
+
+                            String textToSend = "";
+                            for (Text.TextBlock block : visionText.getTextBlocks()) {
+                                Rect boundingBox = block.getBoundingBox();
+                                Point[] cornerPoints = block.getCornerPoints();
+                                String text = block.getText();
+                                textToSend += text;
+
+                                // globalText.concat(text);
+
+                         //       Log.d("TAG", "globalText");
+                           //     Log.d("TAG", globalText);
+
+                                for (Text.Line line : block.getLines()) {
+
+                                    for (Text.Element element : line.getElements()) {
+
+                                    }
+                                }
+                            }
+                           switchActivity(textToSend);
+
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+
+                                    }
+                                });
+    }
+
 
 }
--- app/src/main/java/io/gritacademy/textrecognitionml/MainActivity.java
+++ app/src/main/java/io/gritacademy/textrecognitionml/MainActivity.java
@@ -9,11 +9,14 @@ import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -42,25 +45,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
-
-        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
-        Bitmap bitmap = drawable.getBitmap();
-
-       InputImage inputImage = fromBitmap(bitmap, 0);
-
+        String textToRead =  getIntent().getStringExtra("TEXT_TO_READ");
 
         scanBtn = findViewById(R.id.scanButton);
         editTextMultiLine = findViewById(R.id.editTextMultiLine);
         Button readBtn = findViewById(R.id.readButton);
+
+
+
+
+            if(getIntent() !=null) {
+
+                editTextMultiLine.setText(textToRead);
+            }
+
 
         readBtn.setOnClickListener(this);
         scanBtn.setOnClickListener(v -> {
 
             Intent intent = new Intent(this, LoadImageActivity.class);
             startActivity(intent);
-       //     recognizeText(inputImage);
-         //   editTextMultiLine.setText(text);
+            //     recognizeText(inputImage);
+            //   editTextMultiLine.setText(text);
 
         });
 
@@ -74,49 +80,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
     }
-        private void recognizeText(InputImage image){
 
-            // [START get_detector_default]
-            TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
-            // [END get_detector_default]
+    private void recognizeText(InputImage image) {
 
-            // [START run_detector]
-            Task<Text> result =
-                    recognizer.process(image)
-                            .addOnSuccessListener(visionText -> {
+        // [START get_detector_default]
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        // [END get_detector_default]
 
-                                for (Text.TextBlock block : visionText.getTextBlocks()) {
-                                    Rect boundingBox = block.getBoundingBox();
-                                    Point[] cornerPoints = block.getCornerPoints();
-                                     text = block.getText();
-                                     for (Text.Line line : block.getLines()) {
+        // [START run_detector]
+        Task<Text> result =
+                recognizer.process(image)
+                        .addOnSuccessListener(visionText -> {
 
-                                        for (Text.Element element : line.getElements()) {
+                            for (Text.TextBlock block : visionText.getTextBlocks()) {
+                                Rect boundingBox = block.getBoundingBox();
+                                Point[] cornerPoints = block.getCornerPoints();
+                                text = block.getText();
+                                for (Text.Line line : block.getLines()) {
 
-                                        }
+                                    for (Text.Element element : line.getElements()) {
+
                                     }
                                 }
-                                // [END get_text]
-                                // [END_EXCLUDE]
-                            })
-                            .addOnFailureListener(
-                                    new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
+                            }
+                            // [END get_text]
+                            // [END_EXCLUDE]
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
 
-                                        }
-                                    });
-            // [END run_detector]
-        }
-
+                                    }
+                                });
+        // [END run_detector]
+    }
 
 
     @Override
     public void onClick(View v) {
 
-
         String toRead = editTextMultiLine.getText().toString();
-        toSpeech.speak(toRead, TextToSpeech.QUEUE_FLUSH,null);
+        toSpeech.speak(toRead, TextToSpeech.QUEUE_FLUSH, null);
 
 
     }
@@ -127,4 +132,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         toSpeech.stop();
         toSpeech.shutdown();
     }
+
 }
